4b403b422fa411b000d7b875b2e0da69366b7e2c
byronlove111 <malik.chan94420@gmail.com>
2026-03-02 20:04:18 +0100

    finished main in exo02

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,6 +6,9 @@ static void replace_str(std::string &line, std::string str_to_replace,
                         std::string new_str) {
   size_t pos = line.find(str_to_replace);
   while (pos != std::string::npos) {
+    std::string before = line.substr(0, pos);
+    std::string after = line.substr(pos + str_to_replace.size(), line.size());
+    line = before + new_str + after;
     pos = line.find(str_to_replace, pos + new_str.size());
   }
 }
@@ -28,20 +31,21 @@ int main(int ac, char *av[]) {
 
   if (!raw_file) {
     std::cout << "Error : Unable to open file1: " << av[1] << '\n';
+    return (1);
   }
   if (str_to_replace.empty()) {
     std::cout << "Error : s1 is empty" << '\n';
+    return (1);
   }
   if (new_str.empty()) {
     std::cout << "Error : s2 is empty" << '\n';
+    return (1);
   }
 
   std::string line;
-  int i = 0;
   while (getline(raw_file, line)) {
-    std::cout << "current line : " << line << '\n';
     replace_str(line, str_to_replace, new_str);
-    i++;
+    replace_file << line;
   }
 
   return (0);
