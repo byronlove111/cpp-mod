2e8cabeb00808845c0f2f03ab774f067775ea0ee
byronlove111 <malik.chan94420@gmail.com>
2026-03-02 19:51:45 +0100

    str replace function impl

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 #include <string>
 
+static void replace_str(std::string &line, std::string str_to_replace,
+                        std::string new_str) {
+  size_t pos = line.find(str_to_replace);
+  while (pos != std::string::npos) {
+    pos = line.find(str_to_replace, pos + new_str.size());
+  }
+}
+
 int main(int ac, char *av[]) {
 
   if (ac != 4) {
@@ -12,20 +20,28 @@ int main(int ac, char *av[]) {
     return (1);
   }
 
-  std::string filename = av[1];
-  std::ifstream raw_file(filename);
-  std::ofstream replace_file(filename + ".replace");
-  std::string s1 = av[2];
-  std::string s2 = av[3];
+  std::ifstream raw_file(av[1]);
+  std::string replace_name = std::string(av[1]) + ".replace";
+  std::ofstream replace_file(replace_name.c_str());
+  std::string str_to_replace = av[2];
+  std::string new_str = av[3];
 
   if (!raw_file) {
     std::cout << "Error : Unable to open file1: " << av[1] << '\n';
   }
-  if (s1.empty()) {
+  if (str_to_replace.empty()) {
     std::cout << "Error : s1 is empty" << '\n';
   }
-  if (s2.empty()) {
+  if (new_str.empty()) {
     std::cout << "Error : s2 is empty" << '\n';
+  }
+
+  std::string line;
+  int i = 0;
+  while (getline(raw_file, line)) {
+    std::cout << "current line : " << line << '\n';
+    replace_str(line, str_to_replace, new_str);
+    i++;
   }
 
   return (0);
