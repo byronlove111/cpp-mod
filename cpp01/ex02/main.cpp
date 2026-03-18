e4be6acdee468bb0d7c68e59c405e24b256bc796
byronlove111 <malik.chan94420@gmail.com>
2026-03-19 00:00:30 +0100

    fix CXX and variables names in ex02

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -4,14 +4,14 @@
 int main() {
 
   std::string str = "HI THIS IS BRAIN";
-  std::string *ptr = &str;
-  std::string &ref = str;
+  std::string *stringPTR = &str;
+  std::string &stringREF = str;
 
   std::cout << "Memory address of str: " << &str << '\n';
-  std::cout << "Memory address of ptr: " << ptr << '\n';
-  std::cout << "Memory address of ref: " << &ref << '\n';
+  std::cout << "Memory address of stringPTR: " << stringPTR << '\n';
+  std::cout << "Memory address of stringREF: " << &stringREF << '\n';
 
   std::cout << "Value of str: " << str << '\n';
-  std::cout << "Value of *ptr: " << *ptr << '\n';
-  std::cout << "Value of ref: " << ref << '\n';
+  std::cout << "Value of *stringPTR: " << *stringPTR << '\n';
+  std::cout << "Value of stringREF: " << stringREF << '\n';
 }
